0b91b5465456f2e6474a6ecae0f573348fde9882
minkyu.kim2 <minkyu.kim2@IC-CS233-8.uc.ucalgary.ca>
2022-08-01 10:08:41 -0600

    removed Mortage from the project

--- src/application/Car.java
+++ src/application/Car.java
@@ -4,27 +4,23 @@ public class Car extends Item {
 	private String model; 
 	private String make;
 	private int year; 
-	private Mortgage mortgage;
 	public Car() {
 		super();
 		model = "";
 		make = "";
 		year = 2020;
-		mortgage = new Mortgage();
 	}
 	public Car(String name, double price, String description, int quantity, String model, String make, int year) {
 		super(name, price, description, quantity, "car");
 		this.model = model;
 		this.make = make;
 		this.year = year;
-		mortgage = new Mortgage();
 	}
 	public Car(Item item) {
 		super(item);
 		model = "";
 		make = "";
 		year = 2020;
-		mortgage = new Mortgage();
 	}
 	public String getModel() {
 		return model;
@@ -44,11 +40,5 @@ public class Car extends Item {
 	public void setYear(int year) {
 		this.year = year;
 	}
-	public Mortgage getMortgage() {
-		return mortgage;
-	}
-	public void setMortgage(Mortgage mortgage) {
-		this.mortgage = mortgage;
-	}
-	
+
 }
