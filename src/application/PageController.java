cbbed8d7d85c59e018902b52a6f14c36be1e6c0a
minkyu.kim2 <minkyu.kim2@IC-CS233-23.uc.ucalgary.ca>
2022-08-05 04:19:06 -0600

    completed writing loadFromFile method

--- src/application/PageController.java
+++ src/application/PageController.java
@@ -18,10 +18,10 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PageController {
-	protected Item item; 
+	protected Item item;
 	protected ShoppingList shoppingList;
 	protected Stage applicationStage;
-	
+
 	/**
 	 * Sets the page to the main page
 	 * 
@@ -30,10 +30,11 @@ public class PageController {
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public void goToMainPage(Stage applicationStage, ShoppingList shoppingList) throws FileNotFoundException, IOException {
+	public void goToMainPage(Stage applicationStage, ShoppingList shoppingList)
+			throws FileNotFoundException, IOException {
 		loadPage(null, ShoppingListPageController.getPathToFxml(), applicationStage, shoppingList, "my shopping list");
 	}
-	
+
 	/**
 	 * load the page whose path is specified in the argument
 	 * 
@@ -46,167 +47,131 @@ public class PageController {
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	public PageController loadPage(Item item, String filePath, Stage applicationStage, ShoppingList shoppingList, String pageTitle) throws FileNotFoundException, IOException {
+	public PageController loadPage(Item item, String filePath, Stage applicationStage, ShoppingList shoppingList,
+			String pageTitle) throws FileNotFoundException, IOException {
 		FXMLLoader loader = new FXMLLoader();
 		VBox root = loader.load(new FileInputStream(filePath));
-		Scene scene = new Scene(root,500,400);
-		PageController pageController = loader.getController(); 
+		Scene scene = new Scene(root, 500, 400);
+		PageController pageController = loader.getController();
 		pageController.setItem(item);
 		pageController.setApplicationStage(applicationStage);
 		pageController.setShoppingList(shoppingList);
 		pageController.fillPage();
 		applicationStage.setScene(scene);
 		applicationStage.setTitle(pageTitle);
-		return pageController; 
+		return pageController;
 
 	}
-	
+
 	private void writeToFile() throws IOException {
 		File myObj = new File("src/application/data.txt");
 		myObj.createNewFile();
 		FileWriter myWriter = new FileWriter("src/application/data.txt");
-		String rowData; 
-		
+		String rowData;
+
 		// save items as rows of CSV in the following order:
 		// category, name, price, quantity, description
 		for (Item item : shoppingList.getItems()) {
-			//enclose fields of type string in "" to bypass potential errors with comma when reading the data
-			rowData = "\"" + item.getCategory() + "\""+ "," 
-			+ "\"" + item.getName() + "\""+ "," 
-			+ item.getPrice() + "," 
-			+ item.getQuantity() + "," 
-			+ "\""+ item.getDescription() + "\""; 
-	
+			// enclose fields of type string in "" to bypass potential errors with comma
+			// when reading the data
+			rowData = "\"" + item.getCategory() + "\"" + "," + "\"" + item.getName() + "\"" + "," + item.getPrice()
+					+ "," + item.getQuantity() + "," + "\"" + item.getDescription() + "\"";
+
 			// write additional data for Car object:
 			// model, maker, year
 			if (item.getCategory() == "car") {
-				rowData += "," + "\""+ ((Car)item).getModel()  + "\"" + "," 
-				+ "\""+ ((Car)item).getMake()  + "\""+ "," 
-				+ ((Car)item).getYear();	
-			} 
-			
-			// append a new line character at the end and write the row to the file 
+				rowData += "," + "\"" + ((Car) item).getModel() + "\"" + "," + "\"" + ((Car) item).getMake() + "\""
+						+ "," + ((Car) item).getYear();
+			}
+
+			// append a new line character at the end and write the row to the file
 			rowData += "\n";
 			myWriter.write(rowData);
 		}
 		myWriter.close();
-		System.out.println("Successfully wrote to the file."); 
+		System.out.println("Successfully wrote to the file.");
 	}
-	
-	
-	private void loadFromFile2() throws FileNotFoundException {
-	   Item item; 
-	   String category; 
-	   String name; 
-	   String price;
-	   String quantity; 
-	   String description; 
-	   String model; 
-	   String make; 
-	   String year; 
-		
-		Scanner scanner = new Scanner(new File("src/application/data.txt")); 
-	    while (scanner.hasNextLine()) {
-	    	
-	    	item = new Item();
-	    	
-	        String rowData = scanner.nextLine();
-	        Scanner rowScanner = new Scanner(rowData);
-	        rowScanner.useDelimiter(",");
-	        while (rowScanner.hasNext()) {
-	        	
-	            category = rowScanner.next();
-	            
-	        }
-	        
-	    }
-		
-	}
-	
-	
+
+
 	// source
 	// https://www.baeldung.com/java-csv-file-array#scanner
 	// accessed on August 04 2022
 	private void loadFromFile() throws FileNotFoundException {
-		   ArrayList<Item> items = new ArrayList<Item>();
-		   // category, name, price, quantity, description
-		   Item item; 
-		   String category; 
-		   String name; 
-		   double price;
-		   int quantity; 
-		   String description; 
-		   String model; 
-		   String make; 
-		   String year; 
 
-		   Scanner scanner = new Scanner(new File("src/application/data.txt")); 
-		       while (scanner.hasNextLine()) {
-		    	   List<String> rowData = getRecordFromLine(scanner.nextLine());
-		    	   item = new Item();
-		    	   
-		    	   item.setCategory(rowData.get(0).replace("\"",""));     	  
-		    	   item.setName(rowData.get(1).replace("\"",""));
-		    	   item.setPrice(Double.parseDouble(rowData.get(2)));
-		    	   item.setQuantity(Integer.parseInt(rowData.get(3)));
-		    	   item.setDescription(rowData.get(4).replace("\"",""));
-		    	   
-		    	   // if the category is object, set additional information
-		    	   // model, maker, year
-		    	   if (item.getCategory().toLowerCase() == "car") {
-		    		   ((Car)item).setModel(rowData.get(5));
-		    		   ((Car)item).setMake(rowData.get(6));
-		    		   ((Car)item).setYear(Integer.parseInt(rowData.get(7)));
-		    	   }
-		    	   items.add(item);
-		       }
-		   
-		   
-		   return records;
+		ArrayList<Item> items = new ArrayList<Item>();
+		Item item;
+		Scanner scanner = new Scanner(new File("src/application/data.txt"));
+
+		while (scanner.hasNextLine()) {
+			List<String> rowData = getRecordFromLine(scanner.nextLine());
+			item = new Item();
+
+			// set the following fields to the item object
+			// category, name, price, quantity, description
+			item.setCategory(rowData.get(0).replace("\"", ""));
+			item.setName(rowData.get(1).replace("\"", ""));
+			item.setPrice(Double.parseDouble(rowData.get(2)));
+			item.setQuantity(Integer.parseInt(rowData.get(3)));
+			item.setDescription(rowData.get(4).replace("\"", ""));
+
+			// if the category is object, set additional information:
+			// model, maker, year
+			if (item.getCategory().toLowerCase() == "car") {
+				((Car) item).setModel(rowData.get(5));
+				((Car) item).setMake(rowData.get(6));
+				((Car) item).setYear(Integer.parseInt(rowData.get(7)));
+			}
+			items.add(item);
 		}
 
-	/**
-	 * parses a line of data separated by a comma and returns them as a list of String
-	 * 
-	 * source:
-	 * https://www.baeldung.com/java-csv-file-array#scanner
-	 * accessed on August 04 2022
-	 * 
-	 * @param line
-	 * @return
-	 */
-	private List<String> getRecordFromLine(String line) {
-	    List<String> values = new ArrayList<String>();
-	    try (Scanner rowScanner = new Scanner(line)) {
-	        rowScanner.useDelimiter(",");
-	        while (rowScanner.hasNext()) {
-	            values.add(rowScanner.next());
-	        }
-	    }
-	    return values;
+		this.shoppingList.setItems(items);
 	}
 
-	
-	
-	public void fillPage() throws FileNotFoundException, IOException {
-		
+	/**
+	 * parses a line of data separated by a comma and returns them as a list of
+	 * String
+	 * 
+	 * source: https://www.baeldung.com/java-csv-file-array#scanner accessed on
+	 * August 04 2022
+	 * 
+	 * @param line
+	 * @return List<String>
+	 */
+	private List<String> getRecordFromLine(String line) {
+		List<String> values = new ArrayList<String>();
+		try (Scanner rowScanner = new Scanner(line)) {
+			rowScanner.useDelimiter(",");
+			while (rowScanner.hasNext()) {
+				values.add(rowScanner.next());
+			}
+		}
+		return values;
 	}
-	
+
+	public void fillPage() throws FileNotFoundException, IOException {
+
+	}
+
 	public Item getItem() {
 		return item;
 	}
+
 	public void setItem(Item item) {
 		this.item = item;
 	}
+
 	public ShoppingList getShoppingList() {
 		return shoppingList;
 	}
+
 	public void setShoppingList(ShoppingList shoppingList) {
 		this.shoppingList = shoppingList;
 	}
+
 	public Stage getApplicationStage() {
 		return applicationStage;
 	}
+
 	public void setApplicationStage(Stage applicationStage) {
 		this.applicationStage = applicationStage;
 	}
